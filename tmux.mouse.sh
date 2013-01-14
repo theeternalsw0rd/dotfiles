8d14900ea6dcd50bf6307fcd604dfb53e3512497
Micah Bucy <micah.bucy@the-chapel.org>
2013-01-14 18:23:58 -0500

    put tmux show-window-options commands in background to make linux happy

--- tmux.mouse.sh
+++ tmux.mouse.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 STATE=`tmux show-window-options -g | grep mode-mouse | grep -c on`
 if [ x$STATE == "x1" ]; then
-	tmux setw -g mode-mouse off > /dev/null
-	tmux setw -g mouse-select-pane off > /dev/null
-	tmux setw -g mouse-select-window off > /dev/null
-	tmux setw -g mouse-resize-pane off > /dev/null
+	tmux setw -g mode-mouse off > /dev/null &
+	tmux setw -g mouse-select-pane off > /dev/null &
+	tmux setw -g mouse-select-window off > /dev/null &
+	tmux setw -g mouse-resize-pane off > /dev/null &
 	tmux display-message "Mouse OFF"
 else
-	tmux setw -g mode-mouse on > /dev/null
-	tmux setw -g mouse-select-pane on > /dev/null
-	tmux setw -g mouse-select-window on > /dev/null
-	tmux setw -g mouse-resize-pane on > /dev/null
+	tmux setw -g mode-mouse on > /dev/null &
+	tmux setw -g mouse-select-pane on > /dev/null &
+	tmux setw -g mouse-select-window on > /dev/null &
+	tmux setw -g mouse-resize-pane on > /dev/null &
 	tmux display-message "Mouse ON"
 fi
