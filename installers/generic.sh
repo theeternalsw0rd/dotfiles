68bb93c75fe8914915d23c43a4a39fce97fad3f5
Micah Bucy <micah.bucy@eternalsword.com>
2013-01-13 23:27:20 -0500

    add generic installer

---
+++ installers/generic.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+cd ..
+BASEDIR=`pwd`
+TIMESTAMP=`date +%s`
+
+echo "Entered $BASEDIR"
+
+FILE=.zshrc
+if [ -e "$HOME/$FILE" ]; then
+	if [ -L "$HOME/$FILE" ]; then
+		SYMLINK=`readlink "$HOME/$FILE"`
+		unlink "$HOME/$FILE"
+		echo "Removed link from ~/$FILE to $SYMLINK"
+	else
+		mv "$HOME/$FILE" "$HOME/$FILE.$TIMESTAMP.bak"
+		echo "Existing ~/$FILE moved to ~/$FILE.$TIMESTAMP.bak"
+	fi
+fi
+ln -f -s "$BASEDIR/$FILE" "$HOME/$FILE"
+echo "$FILE installed"
+
+FILE=.vimrc
+if [ -e "$HOME/$FILE" ]; then
+	if [ -L "$HOME/$FILE" ]; then
+		SYMLINK=`readlink "$HOME/$FILE"`
+		unlink "$HOME/$FILE"
+		echo "Removed link from ~/$FILE to $SYMLINK"
+	else
+		mv "$HOME/$FILE" "$HOME/$FILE.$TIMESTAMP.bak"
+		echo "Existing ~/$FILE moved to ~/$FILE.$TIMESTAMP.bak"
+	fi
+fi
+ln -f -s "$BASEDIR/$FILE" "$HOME/$FILE"
+echo "$FILE installed"
+
+FILE=.vim
+if [ -e "$HOME/$FILE" ]; then
+	if [ -L "$HOME/$FILE" ]; then
+		SYMLINK=`readlink "$HOME/$FILE"`
+		unlink "$HOME/$FILE"
+		echo "Removed link from ~/$FILE to $SYMLINK"
+	else
+		mv "$HOME/$FILE" "$HOME/$FILE.$TIMESTAMP.bak"
+		echo "Existing ~/$FILE moved to ~/$FILE.$TIMESTAMP.bak"
+	fi
+fi
+ln -f -s "$BASEDIR/$FILE" "$HOME/$FILE"
+echo "$FILE installed"
+
+FILE=.tmux.conf
+if [ -e "$HOME/$FILE" ]; then
+	if [ -L "$HOME/$FILE" ]; then
+		SYMLINK=`readlink "$HOME/$FILE"`
+		unlink "$HOME/$FILE"
+		echo "Removed link from ~/$FILE to $SYMLINK"
+	else
+		mv "$HOME/$FILE" "$HOME/$FILE.$TIMESTAMP.bak"
+		echo "Existing ~/$FILE moved to ~/$FILE.$TIMESTAMP.bak"
+	fi
+fi
+ln -f -s "$BASEDIR/$FILE" "$HOME/$FILE"
+echo "$FILE installed"
+
+FILE=.oh-my-zsh
+if [ -e "$HOME/$FILE" ]; then
+	if [ -L "$HOME/$FILE" ]; then
+		SYMLINK=`readlink "$HOME/$FILE"`
+		unlink "$HOME/$FILE"
+		echo "Removed link from ~/$FILE to $SYMLINK"
+	else
+		mv "$HOME/$FILE" "$HOME/$FILE.$TIMESTAMP.bak"
+		echo "Existing ~/$FILE moved to ~/$FILE.$TIMESTAMP.bak"
+	fi
+fi
+ln -f -s "$BASEDIR/$FILE" "$HOME/$FILE"
+echo "$FILE installed"
+
+FILE=.local.zsh
+if [ ! -e "$HOME/$FILE" ]; then
+	echo "# local zsh configuration" > $HOME/$FILE
+	echo "local zsh configuration ~/$FILE created"
+else
+	echo "leaving ~/$FILE intact"
+fi
