af1330fed48e0de7fa41c699c257d5a70459ea0d
Micah Bucy <micah.bucy@the-chapel.org>
2013-01-24 17:57:07 -0500

    start tmux automatically but only in an ssh session

--- zshrc.zsh
+++ zshrc.zsh
@@ -1,3 +1,14 @@
+if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+	SESSION_TYPE=remote/ssh
+	# many other tests omitted
+else
+	case $(ps -o comm= -p $PPID) in
+		sshd|*/sshd) SESSION_TYPE=remote/ssh;;
+	esac
+fi
+if [ "$SESSION_TYPE" = "remote/ssh" ] && [ -z "$TMUX" ]; then
+	tmux a || tmux
+fi
 # Path to your oh-my-zsh configuration.
 ZSH=$HOME/.scripts/zsh/oh-my-zsh
 
