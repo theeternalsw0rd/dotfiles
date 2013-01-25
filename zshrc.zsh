d7dbaeb35c92842bd8177552b9094f1e29132181
Micah Bucy <micah.bucy@eternalsword.com>
2013-01-24 20:47:22 -0500

    add zsh syntax highlighting

--- zshrc.zsh
+++ zshrc.zsh
@@ -39,7 +39,7 @@ DISABLE_AUTO_TITLE="true"
 # Which plugins would you like to load? (plugins can be found in ~/.scripts/zsh/oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.scripts/zsh/oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git)
+plugins=(git zsh-syntax-highlighting)
 
 source $ZSH/oh-my-zsh.sh
 
