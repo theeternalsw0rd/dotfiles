9a7f37c4c92abd9382b32519c1adf5553c09be7f
Micah Bucy <micah.bucy@eternalsword.com>
2013-01-25 23:35:10 -0500

    a zsh powerline that actually works

--- zshrc.zsh
+++ zshrc.zsh
@@ -21,7 +21,7 @@ ZSH=$HOME/.scripts/zsh/oh-my-zsh
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
-ZSH_THEME="frosted"
+ZSH_THEME="powerline"
 
 # Set to this to use case-sensitive completion
 # CASE_SENSITIVE="true"
