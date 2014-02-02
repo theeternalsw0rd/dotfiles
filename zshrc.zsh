6a2b56163df4926a4c79719db36639b8bdbfd54d
Micah Bucy <micah.bucy@eternalsword.com>
2014-02-01 20:42:47 -0500

    switch to ys zsh theme

--- zshrc.zsh
+++ zshrc.zsh
@@ -22,7 +22,7 @@ ZSH=$HOME/.scripts/zsh/oh-my-zsh
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
-ZSH_THEME="powerline"
+ZSH_THEME="ys"
 
 # Set to this to use case-sensitive completion
 # CASE_SENSITIVE="true"
