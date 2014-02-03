17e2e14859657602713e659c56a3576f142e43fd
Micah Bucy <micah.bucy@eternalsword.com>
2014-02-02 20:32:02 -0500

    better string padding

--- scripts/tmux/status.sh
+++ scripts/tmux/status.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+function lpad {
+	word="$1"
+	while [ ${#word} -lt $2 ]; do
+		if [[ "$4" == "right" ]]; then
+			word="$word$3";
+		else
+			word="$3$word";
+		fi
+	done;
+	echo "$word";
+}
 if [[ "$1" == "times" ]]; then
 	uptime=`~/.scripts/helper/uptime.sh`
 	date=`date +%Y-%m-%d`
@@ -6,7 +17,7 @@ if [[ "$1" == "times" ]]; then
 	echo "$uptime  $date  $time"
 fi
 if [[ "$1" == "host" ]]; then
-	ip=`dig +short myip.opendns.com @resolver1.opendns.com | sed -e :a -e 's/^.\{1,15\}$/ & /;ta'`
-	hostname=`hostname | sed -e :a -e 's/^.\{1,8\}$/ &/;ta'`
-	echo "$ip  $hostname"
+	ip=`dig +short myip.opendns.com @resolver1.opendns.com`
+	hostname=`hostname`
+	echo "`lpad $ip 15 ' ' right`  `lpad $hostname 8 ' '`"
 fi
