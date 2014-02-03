493ac8034e979f1ae0396bf0c3daed4814cf8907
Micah Bucy <micah.bucy@eternalsword.com>
2014-02-02 20:19:52 -0500

    pad host and ip

--- scripts/tmux/status.sh
+++ scripts/tmux/status.sh
@@ -3,10 +3,10 @@ if [[ "$1" == "times" ]]; then
 	uptime=`~/.scripts/helper/uptime.sh`
 	date=`date +%Y-%m-%d`
 	time=`date +%R`
-	echo $uptime"  "$date"  "$time
+	echo "$uptime  $date  $time"
 fi
 if [[ "$1" == "host" ]]; then
-	ip=`dig +short myip.opendns.com @resolver1.opendns.com`
-	hostname=`hostname | while read i; do printf "%10s\n" "$i"; done`
-	echo $ip"  "$hostname
+	ip=`dig +short myip.opendns.com @resolver1.opendns.com | sed -e :a -e 's/^.\{1,15\}$/ & /;ta'`
+	hostname=`hostname | sed -e :a -e 's/^.\{1,8\}$/ &/;ta'`
+	echo "$ip  $hostname"
 fi
