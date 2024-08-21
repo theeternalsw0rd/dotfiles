ac987500c479d4eb398c0c6650fc041175d61cc9
Micah Bucy <micah.bucy@theeternalsw0rd.rocks>
2024-08-21 16:00:04 -0400

    add fastfetch to zsh launch

--- zshrc.zsh
+++ zshrc.zsh
@@ -24,6 +24,10 @@ antigen bundle zsh-users/zsh-syntax-highlighting
 
 antigen apply
 
+if [ `command -v fastfetch` ]; then
+  fastfetch
+fi
+
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
