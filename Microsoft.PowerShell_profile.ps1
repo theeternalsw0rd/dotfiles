0f9bb8a7a278b757ed9a5b1eeca12d71f5aec3ca
Micah Bucy <micah.bucy@theeternalsw0rd.rocks>
2025-06-15 14:47:13 -0400

    add powershell profile script

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,75 @@
+if (Get-Command starship -ErrorAction SilentlyContinue) {
+  Invoke-Expression (&starship init powershell)
+}
+else {
+  Write-Host "you should install starship"
+}
+if (Get-Module -ListAvailable -Name cd-extras) {
+  Import-Module cd-extras
+} 
+else {
+  Write-Host "you should install module cd-extras"
+}
+
+function head {
+  [CmdletBinding()]
+  Param([Parameter(ValueFromPipeline=$true)] $pipeline, [Int] $n = 20)
+  Begin {
+    $inputPipe = @()
+    [System.Collections.ArrayList]$inputPipeList = $inputPipe
+  }
+  Process {
+    $inputPipeList.Add($pipeline) | out-null
+  }
+  End {
+    $inputPipeList | Select-Object -first $n 
+  }
+}
+
+function tail {
+  [CmdletBinding()]
+  Param([Parameter(ValueFromPipeline=$true)] $pipeline, [Int] $n = 20)
+  Begin {
+    $inputPipe = @()
+    [System.Collections.ArrayList]$inputPipeList = $inputPipe
+  }
+  Process {
+    $inputPipeList.Add($pipeline) | out-null
+  }
+  End {
+    $inputPipeList | Select-Object -last $n
+  }
+}
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+else {
+  Write-Host "choco is not installed yet."
+}
+
+Set-Alias -Name which -Value Get-Command
+
+# rbenv for Windows
+$env:RBENV_ROOT = "C:\Ruby-on-Windows"
+$rbenvScript = "$env:RBENV_ROOT\rbenv\bin\rbenv.ps1"
+
+if (Test-Path($rbenvScript)) {
+  # Not easy to download on Github?
+  # Use a custom mirror!
+  # $env:RBENV_USE_MIRROR = "https://abc.com/abc-<version>"
+
+  & "$rbenvScript" init
+}
+
+if (Get-Command "eza.exe" -ErrorAction SilentlyContinue) {
+  Remove-Alias ls
+  function ls { eza --icons $args }
+  Set-Alias -Name winls -Value Get-ChildItem
+}
+
+else {
+  Write-Host "You have not installed eza yet, or it is not in your PATH."
+}
